54ed71ca75c7b5a853692a920631ed3b2277e971
Zxc-Ibrokhim <161280335+Zxc-Ibrokhim@users.noreply.github.com>
2024-05-24 08:12:34 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+const firstNumber = prompt("Введите первое число:");
+const secondNumber = prompt("Введите второе число:");
+
+let num1 = firstNumber + secondNumber;
+let num2 = firstNumber - secondNumber;
+let num3 = firstNumber * secondNumber;
+let num4 = firstNumber / secondNumber;
+
+alert(`Сумма: ${num1}
+Разность: ${num2}
+Произведение: ${num3}
+Частное: ${num4}`);
+
